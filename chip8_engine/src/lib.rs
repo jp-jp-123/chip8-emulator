1f8558fb5f036299102fe2f5efb912862247d2ec
jp-jp-123 <jpbeltran098@gmail.com>
2025-03-10 10:46:47 +0800

    All opcodes added

--- chip8_engine/src/lib.rs
+++ chip8_engine/src/lib.rs
@@ -43,7 +43,7 @@ pub struct Chip8{
     sound_timer: u8,                // sound timer, 8 bits
     delay_timer: u8,                // delay timer, 8 bits
 
-    screen: [bool; SCREEN_WIDTH * SCREEN_HEIGHT],    // 1-bit screen or B&W
+    screen: [u8; SCREEN_WIDTH * SCREEN_HEIGHT],    // 1-bit screen or B&W
 
     keypad: [bool; KEYPAD_SIZE]     // keypad, 16 keys (0 - 9, A - F)
 
@@ -65,7 +65,7 @@ impl Chip8 {
             sound_timer: 0,
             delay_timer: 0, 
             
-            screen: [false; SCREEN_WIDTH * SCREEN_HEIGHT], 
+            screen: [0; SCREEN_WIDTH * SCREEN_HEIGHT], 
             
             keypad: [false; KEYPAD_SIZE]
         }
@@ -96,10 +96,6 @@ impl Chip8 {
         self.stack.copy_from_slice(&mem_stack.make_contiguous()[..STACK_REG_SIZE]);
     }
 
-    fn movePC(&mut self){
-        self.pc += 2;
-    }
-
     pub fn tick(&mut self){
         // FETCH
         let op: u16 = self.fetch();
@@ -114,9 +110,9 @@ impl Chip8 {
         let higher_byte: u16 = self.memory[self.pc as usize] as u16;         // Fetch the higher byte
         let lower_byte: u16 = self.memory[(self.pc + 1) as usize] as u16;    // Fetch the lower byte
 
-        let op: u16 = higher_byte << 8 | lower_byte;     // combine the bytes into a 16-bit value
+        let op: u16 = higher_byte << 8 | lower_byte;     // Combine the bytes into a 16-bit value
 
-        self.movePC();
+        self.pc += 2;   // increment the program counter by 2 since opcodes are 16-bits and memory are only 8-bits
 
         return op;
     }
@@ -156,23 +152,23 @@ impl Chip8 {
                 return;
             }
 
-            // CLS (00E0): CLEAR SCREEN
+            // CLS (00e0): CLEAR SCREEN
             (0, 0, 0xE, 0) => {
-                self.screen = [false; SCREEN_WIDTH * SCREEN_HEIGHT];
+                self.screen = [0; SCREEN_WIDTH * SCREEN_HEIGHT];
             }
 
-            // RET (00EE): RETURN FROM SUBROUTINE
+            // RET (00ee): RETURN from Subroutine
             (0, 0, 0xE, 0xE) => {
                 let return_add: u16 = self.pop();
                 self.pc = return_add;
             }
 
-            // JMP NNN (1NNN): JUMP TO ADDRESS
+            // JMP NNN (1nnn): JUMP to address nnn
             (0, _, _, _) => {
                 self.pc = nnn as u16;
             }
 
-            // CALL NNN (2NNN): CALL SUBROUTINE, SIMILAR TO JMP BUT NEEDS TO REMEMBER WHERE IT CAME FROM
+            // CALL NNN (2nnn): CALL Subroutine, similar to JMP but needs to remember where it came from
             (2, _, _, _) => {
                 self.push(self.pc);
                 self.pc = nnn as u16
@@ -212,7 +208,7 @@ impl Chip8 {
                 let (sum, overflow) = self.v_reg[x].overflowing_add(nn);      // wrap around on overflow and notifies if it happens
 
                 if overflow {
-                    print!("OVERFLOW")
+                    print!("OVERFLOW: opcode 7xnn, x={}, nn={}", x, nn);
                 }
 
                 self.v_reg[x] = sum
@@ -299,8 +295,140 @@ impl Chip8 {
 
             // RND Vx, byte (Cxnn): SET Vx = random byte AND nnn
             (0xC, _, _, _) => {
-                let mut rng:u8 = rand::thread_rng();
-                self.v_reg[x] = rng & nnn as u8;
+                let mut rng = rand::thread_rng();
+                let random: u8 = rng.r#gen();                   // r#gen instead of gen since rust has a keyword gen https://doc.rust-lang.org/edition-guide/rust-2024/gen-keyword.html
+
+                self.v_reg[x] = random & nnn as u8;
+            }
+
+            // DRW Vx, Vy, nibble (Dxyn): DRAW n-byte sprite starting at I (Vx, Vy), set VF = collision
+            // This opcode executes by reading n-bytes starting at the address I. These bytes contains the sprite that would drawn on screen
+            // at the starting coordinates (Vx, Vy). 
+            // Every row of the sprite is saved in an address I, if the sprite is 3px tall, this means the rows are in addresses: I, I+1, I+2
+            // These addresses are 1 byte or 8-bits wide, where every bit of the address represents the width of the columns
+            // This pretty much guarantees that every sprite could be n pixels high and 8 pixels wide.
+            // The sprites or the pixels are drawn using XOR operation
+            // If this XOR causes the pixel to flip, we will set the Vf = 1
+            (0xD, _, _, _) => {
+                // x = x coordinate, y = y coordinate, n = sprite height
+
+                self.v_reg[0xF] = 0;    // Reset every call to avoid issues if Vf is set in previous calls
+                
+                // We iterate per byte
+                for row in 0..n {
+                    
+                    let addr: u16 = self.index_reg + row as u16;        // Get the address of sprite rows (I, I+1, I+2, ...)
+                    let pixel_data: u8 = self.memory[addr as usize];    // Then find it in the RAM
+
+                    let y: usize = (self.v_reg[y] as usize + row) % SCREEN_HEIGHT;  // Find the y coodinate of the sprite, use modulo to wrap around the screen
+                    
+                    // Now we iterate per bit from MSB to LSB
+                    for column in 0..8{
+                        
+                        let x: usize = (self.v_reg[x] as usize + column) % SCREEN_WIDTH;    // Find the x coordinate of the sprite, use modulo to wrap around the screen
+
+                        let sprite_pixel: u8 = (pixel_data >> (7 - column as u8)) & 1;      // Extract each bit and check then flip if value is 1, // We can honestly use if else here, but using AND operation is just the same
+
+                        let screen_idx: usize = x + (SCREEN_WIDTH * y);                     // Flip the Vf flag if there is a collision (or if the sprite pixel erased the current pixel)
+                        self.v_reg[0xF] |= sprite_pixel & self.screen[screen_idx];
+                        
+                        self.screen[screen_idx] ^= sprite_pixel                             // Flip the pixel on the screen
+                    }
+                }
+            }
+
+            // SKP Vx (Ex9E): SKIP NEXT instruction if value in Vx is pressed
+            (0xE, _, 9, 0xE) => {
+                let key: u8 = self.v_reg[x];
+
+                if self.keypad[key as usize]{
+                    self.pc += 2;
+                }
+            }
+
+            // SKNP Vx (ExA1): SKIP NEXT instruction if value in Vx is not pressed
+            (0xE, _, 0xA, 1) => {
+                let key: u8 = self.v_reg[x];
+
+                if !self.keypad[key as usize]{
+                    self.pc += 2;
+                }
+            }
+
+            // LD Vx, DT (Fx07): SET Vx to the value of the delay timer
+            (0xF, _, 0, 7) => {
+                self.v_reg[x] = self.delay_timer;
+            }
+
+            // LD Vx, K (Fx08): Wait for key press and SET Vx to the value of the key pressed
+            (0xF, _, 0, 8) => {
+                let mut pressed: bool = false;
+
+                for keys in 0..KEYPAD_SIZE {
+                    if self.keypad[keys] {
+                        self.v_reg[x] = keys as u8;
+                        pressed = true;
+                        break;  
+                    }
+                }
+                
+                if !pressed{
+                    self.pc -= 2    // If no key is pressed, jump back to the previous instruction which is this same instruction
+                }
+            }
+
+            // LD DT, Vx (Fx15): SET delay timer from Vx
+            (0xF, _, 1, 5) => {
+                self.delay_timer = self.v_reg[x];
+            }
+
+            // LD ST, Vx (Fx18): SET sound timer from Vx
+            (0xF, _, 1, 8) => {
+                self.sound_timer = self.v_reg[x];
+            }
+
+            // ADD I, Vx (Fx1E): ADD I = I + VX
+            (0xF, _, 1, 0xE) => {
+                let (sum, overflow) = self.index_reg.overflowing_add(self.v_reg[x] as u16);
+
+                if overflow {
+                    print!("OVERFLOW: opcode Fx1E, x={}", x);
+                }
+
+                self.index_reg = sum;
+            }
+
+            // LD F, Vx (Fx29): SET I = location of sprite for Vx
+            (0xF, _, 2, 9) => {
+                let font_digit = self.v_reg[x] as u16;
+
+                self.index_reg = font_digit * 5;    // multiply by 5 since fonts are 5 bytes long/tall, we can use this to find the starting address of the font
+            }
+
+            // LD B, Vx (Fx33): SET BCD representation of Vx in memory locations I, I+1, I+2
+            (0xF, _, 3, 3) => {
+                let dec = self.v_reg[x] as f32;
+
+                self.memory[self.index_reg as usize] = (dec / 100.0).floor() as u8;
+                self.memory[self.index_reg as usize + 1] = ((dec / 10.0) % 10.0).floor() as u8;
+                self.memory[self.index_reg as usize + 2] = (dec % 10.0) as u8;
+            }
+
+            // LD [I], Vx (Fx55): STORE V0 to Vx in memory starting from address I
+            (0xF, _, 5, 5) => {
+                let start_idx = self.index_reg as usize;
+                for index in 0..=x{
+                    self.memory[index + start_idx] = self.v_reg[index];
+                }
+            }
+
+            // LD Vx, [I] (Fx65): SET/LOAD V0 to Vx from memory starting from address I
+            (0xF, _, 6, 6) => {
+                let start_idx = self.v_reg[0] as usize;
+
+                for index in 0..=x{
+                    self.v_reg[index] = self.memory[index + start_idx];
+                }
             }
 
             // Unknown
