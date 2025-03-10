1af69baea1fd66ae5dea2e6777c42db65a6f8573
jp-jp-123 <jpbeltran098@gmail.com>
2025-03-10 23:34:05 +0800

    Revert "adjusted the screen height and width as public"
    This reverts commit 31963e8187b5290b73b7f76fd51fc4e71a359081.

--- chip8_engine/src/lib.rs
+++ chip8_engine/src/lib.rs
@@ -1,7 +1,7 @@
 use rand::Rng;
 
-pub const SCREEN_WIDTH: usize = 64;
-pub const SCREEN_HEIGHT: usize = 32;
+const SCREEN_WIDTH: usize = 64;
+const SCREEN_HEIGHT: usize = 32;
 
 const RAM_SIZE: usize = 4096;
 const V_REG_SIZE: usize = 16;
