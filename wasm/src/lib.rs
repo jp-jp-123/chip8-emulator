faf7190a79412be87b636ed06dd65d74b97d0625
jp-jp-123 <jpbeltran098@gmail.com>
2025-03-14 10:55:35 +0800

    added more functions

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -1,16 +1,21 @@
 use chip8_engine::*;
-use wasm-bindgen::prelude::*;
+use wasm_bindgen::prelude::*;
+
+use web_sys::KeyboardEvent;
+use js_sys::Uint8Array;
 
 #[wasm_bindgen]
 pub struct Chip8EngineWasm {
-    chip8: Chip8;
+    chip8: Chip8,
 }
 
 #[wasm_bindgen]
 impl Chip8EngineWasm {
     #[wasm_bindgen(constructor)]
     pub fn new() -> Self {
-         chip8 = Chip8::new(true);
+        Self{
+            chip8: Chip8::new(),
+        }
     }
 
     #[wasm_bindgen]
@@ -22,5 +27,50 @@ impl Chip8EngineWasm {
     pub fn timers(&mut self){
         self.chip8.timers();
     }
+
+    #[wasm_bindgen]
+    pub fn reset(&mut self){
+        self.chip8.reset();
+    }
+
+    #[wasm_bindgen]
+    pub fn keypress(&mut self, evt: KeyboardEvent, pressed: bool) {
+        let key = evt.key();
+        if let Some(k) = key2btn(&key){
+            self.chip8.set_keypad(k, pressed);
+        }
+    }
+
+    #[wasm_bindgen]
+    pub fn load_rom(&mut self, rom: Uint8Array){
+        self.chip8.load_rom(&rom.to_vec());
+    }
+
+    pub fn draw_screen(&mut self, scale: usize){
+        //TODO
+    }
 }
+
+fn key2btn(key: &str) -> Option<usize> {
+    match key {
+        "1" => Some(0x1),
+        "2" => Some(0x2),
+        "3" => Some(0x3),
+        "4" => Some(0xC),
+        "q" => Some(0x4),
+        "w" => Some(0x5),
+        "e" => Some(0x6),
+        "r" => Some(0xD),
+        "a" => Some(0x7),
+        "s" => Some(0x8),
+        "d" => Some(0x9),
+        "f" => Some(0xE),
+        "z" => Some(0xA),
+        "x" => Some(0x0),
+        "c" => Some(0xB),
+        "v" => Some(0xF),
+        _ =>   None,
+    }
+}
+
 
